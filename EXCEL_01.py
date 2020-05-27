dbe94984d10ce666288912b4a3d9996468e37c4b
J7_MacBookPro <castelanojct@fab.mil.br>
2020-05-27 13:38:59 -0300

    primeiro commit

---
+++ EXCEL_01.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_excel("EXCEL_011.xls")
+print(df.head(3))
+pk = pd.read_csv("pokemon_data.csv")
+print(pk.head(2))
+pktxt = pd.read_csv("pokemon_data.txt", delimiter="\t")
+print(pktxt.head(3))
+print(df.columns)
